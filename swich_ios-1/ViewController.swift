b79730bbbe32895eeb66055c63e7eaa583e1360a
Zhng990317 <zbq990317@yeah.net>
2018-11-16 21:05:44 +0800

    SWift_ios(2)

--- swich_ios-1/ViewController.swift
+++ swich_ios-1/ViewController.swift
@@ -13,64 +13,72 @@ class ViewController:
     
     var temp : Double = 0
     var flag : Int = 0
-
-    @IBOutlet weak var asd: UITextField!
-    
-    @IBAction func dot(_ sender: Any) {
-        asd.text = asd.text! + "."
-    }
-    
-    @IBAction func b0(_ sender: Any) {asd.text = asd.text! + "0"    }
-    @IBAction func b1(_ sender: Any) {asd.text = asd.text! + "1"    }
-    @IBAction func b2(_ sender: Any) {asd.text = asd.text! + "2"    }
-    @IBAction func b3(_ sender: Any) {asd.text = asd.text! + "3"    }
-    @IBAction func b4(_ sender: Any) {asd.text = asd.text! + "4"    }
-    @IBAction func b5(_ sender: Any) {asd.text = asd.text! + "5"    }
-    @IBAction func b6(_ sender: Any) {asd.text = asd.text! + "6"    }
-    @IBAction func b7(_ sender: Any) {asd.text = asd.text! + "7"    }
-    @IBAction func b8(_ sender: Any) {asd.text = asd.text! + "8"    }
-    @IBAction func b9(_ sender: Any) {asd.text = asd.text! + "9"    }
+             
+    @IBOutlet weak var zxc: UITextField!
+             
+    @IBAction func dot(_ sender: Any) {zxc.text = zxc.text! + "."   }
+    @IBAction func b0(_ sender: Any) {zxc.text = zxc.text! + "0"    }
+    @IBAction func b1(_ sender: Any) {zxc.text = zxc.text! + "1"    }
+    @IBAction func b2(_ sender: Any) {zxc.text = zxc.text! + "2"    }
+    @IBAction func b3(_ sender: Any) {zxc.text = zxc.text! + "3"    }
+    @IBAction func b4(_ sender: Any) {zxc.text = zxc.text! + "4"    }
+    @IBAction func b5(_ sender: Any) {zxc.text = zxc.text! + "5"    }
+    @IBAction func b6(_ sender: Any) {zxc.text = zxc.text! + "6"    }
+    @IBAction func b7(_ sender: Any) {zxc.text = zxc.text! + "7"    }
+    @IBAction func b8(_ sender: Any) {zxc.text = zxc.text! + "8"    }
+    @IBAction func b9(_ sender: Any) {zxc.text = zxc.text! + "9"    }
     
     @IBAction func jia(_ sender: Any) {
-        temp = Double(asd.text!)!
-        asd.text = ""
+        temp = Double(zxc.text!)!
+        zxc.text = ""
         flag = 1
+        
     }
     @IBAction func jian(_ sender: Any) {
-        temp = Double(asd.text!)!
-        asd.text = ""
+        temp = Double(zxc.text!)!
+        zxc.text = ""
         flag = 2
     }
     @IBAction func cheng(_ sender: Any) {
-        temp = Double(asd.text!)!
-        asd.text = ""
+        temp = Double(zxc.text!)!
+        zxc.text = ""
         flag = 3
     }
     @IBAction func chu(_ sender: Any) {
-        temp = Double(asd.text!)!
-        asd.text = ""
+        temp = Double(zxc.text!)!
+        zxc.text = ""
         flag = 4
     }
+    
+    @IBAction func baifen(_ sender: Any) {
+        temp = Double(zxc.text!)! / 100
+        zxc.text = "\(temp)"
+    }
+    @IBAction func zhengfu(_ sender: Any) {
+        temp = Double(zxc.text!)! * (-1)
+        zxc.text = "\(temp)"
+    }
+    
     @IBAction func dengyu(_ sender: Any) {
         if (flag == 1){
-            temp = temp + Double(asd.text!)!
-            asd.text = "\(temp)"
+            temp = temp + Double(zxc.text!)!
+            zxc.text = "\(temp)"
         }
         if (flag == 2){
-            temp = temp - Double(asd.text!)!
-            asd.text = "\(temp)"
+            temp = temp -  Double(zxc.text!)!
+            zxc.text = "\(temp)"
         }
         if (flag == 3){
-            temp = temp * Double(asd.text!)!
-            asd.text = "\(temp)"
+            temp = temp * Double(zxc.text!)!
+            zxc.text = "\(temp)"
         }
         if (flag == 4){
-            temp = temp / Double(asd.text!)!
-            asd.text = "\(temp)"
+            temp = temp / Double(zxc.text!)!
+            zxc.text = "\(temp)"
         }
     }
     @IBAction func ac(_ sender: Any) {
-        asd.text = ""
+        zxc.text = ""
     }
     
     override func viewDidLoad() {
