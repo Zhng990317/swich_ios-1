ced173b611ee385f49c7cb423f2266f06ce052b9
Zhng990317 <zbq990317@yeah.net>
2019-01-05 09:48:38 +0800

    1.5

--- swich_ios-1/ViewController.swift
+++ swich_ios-1/ViewController.swift
@@ -8,46 +8,240 @@
 
 import UIKit
 
-class ViewController:
-    UIViewController {
+class ViewController:UIViewController {
     
-    var temp : Double = 0
-    var flag : Int = 0
-             
+    
     @IBOutlet weak var zxc: UITextField!
-             
-    @IBAction func dot(_ sender: Any) {zxc.text = zxc.text! + "."   }
-    @IBAction func b0(_ sender: Any) {zxc.text = zxc.text! + "0"    }
-    @IBAction func b1(_ sender: Any) {zxc.text = zxc.text! + "1"    }
-    @IBAction func b2(_ sender: Any) {zxc.text = zxc.text! + "2"    }
-    @IBAction func b3(_ sender: Any) {zxc.text = zxc.text! + "3"    }
-    @IBAction func b4(_ sender: Any) {zxc.text = zxc.text! + "4"    }
-    @IBAction func b5(_ sender: Any) {zxc.text = zxc.text! + "5"    }
-    @IBAction func b6(_ sender: Any) {zxc.text = zxc.text! + "6"    }
-    @IBAction func b7(_ sender: Any) {zxc.text = zxc.text! + "7"    }
-    @IBAction func b8(_ sender: Any) {zxc.text = zxc.text! + "8"    }
-    @IBAction func b9(_ sender: Any) {zxc.text = zxc.text! + "9"    }
+    var temp : Double = 0.0
+    var flag = 0
+    var result :Double = 0.0
+
+    @IBAction func dot(_ sender: Any) {
+            zxc.text = zxc.text! + "."
+    }
+    @IBAction func b0 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "0"
+        
+    }
+    @IBAction func b1 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "1"
+        
+    }
+    @IBAction func b2 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "2"
+    }
+    @IBAction func b3 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "3"
+        
+    }
+    @IBAction func b4 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "4"
+        
+    }
+    @IBAction func b5 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "5"
+        
+    }
+    @IBAction func b6 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "6"
+        
+    }
+    @IBAction func b7 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "7"
+        
+    }
+    @IBAction func b8 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "8"
+        
+    }
+    @IBAction func b9 (_ sender: Any) {
+        if zxc.text == "0"
+        {
+            zxc.text = ""
+        }
+        zxc.text = zxc.text! + "9"
+        
+    }
     
     @IBAction func jia(_ sender: Any) {
-        temp = Double(zxc.text!)!
-        zxc.text = ""
+        
+        if zxc.text != "" {
+            if flag == 2 {
+                result = temp - Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 3 {
+                result = temp * Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 4 {
+                result = temp / Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+        }
+        if flag == 1 {
+            if zxc.text == "" {
+                zxc.text = "0"
+            }
+            temp = temp + Double(zxc.text!)!
+            zxc.text = "\(temp)"
+        }
+        else {
+            temp = Double(zxc.text!)!
+            zxc.text = ""
+        }
         flag = 1
         
     }
     @IBAction func jian(_ sender: Any) {
-        temp = Double(zxc.text!)!
-        zxc.text = ""
-        flag = 2
+       
+        if zxc.text != "" {
+            if flag == 1 {
+                result = temp + Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 3 {
+                result = temp * Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 4 {
+                result = temp / Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+        }
+        if flag == 2 {
+            if zxc.text == "" {
+                zxc.text = "0"
+            }
+            temp = temp - Double(zxc.text!)!
+            zxc.text = "\(temp)"
+        }
+        else {
+            temp = Double(zxc.text!)!
+            zxc.text = ""
+        }
+         flag = 2
+        
     }
     @IBAction func cheng(_ sender: Any) {
-        temp = Double(zxc.text!)!
-        zxc.text = ""
+        
+        if zxc.text != ""{
+            if flag == 1 {
+                result = temp + Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 2 {
+                result = temp - Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 4 {
+                result = temp / Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+        }
+        if flag == 3 {
+            if zxc.text == "" {
+                zxc.text = "1"
+            }
+            temp = temp * Double(zxc.text!)!
+            zxc.text = "\(temp)"
+        }
+        else {
+            temp = Double(zxc.text!)!
+            zxc.text = ""
+        }
         flag = 3
+        
     }
     @IBAction func chu(_ sender: Any) {
-        temp = Double(zxc.text!)!
-        zxc.text = ""
-        flag = 4
+       
+        if zxc.text != "" {
+            if flag == 1 {
+                result = temp + Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 2 {
+                result = temp - Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+            if flag == 3 {
+                result = temp * Double(zxc.text!)!
+                zxc.text = "\(temp)"
+                zxc.text = ""
+                
+            }
+        }
+        if flag == 4 {
+            if zxc.text == "" {
+                zxc.text = "1"
+            }
+            temp = temp / Double(zxc.text!)!
+            zxc.text = "\(temp)"
+        }
+        else {
+            temp = Double(zxc.text!)!
+            zxc.text = ""
+        }
+         flag = 4
+        
     }
     
     @IBAction func baifen(_ sender: Any) {
@@ -60,32 +254,31 @@ class ViewController:
     }
     
     @IBAction func dengyu(_ sender: Any) {
-        if (flag == 1){
+        if flag == 1 {
             temp = temp + Double(zxc.text!)!
-            zxc.text = "\(temp)"
         }
-        if (flag == 2){
+        if flag == 2 {
             temp = temp -  Double(zxc.text!)!
-            zxc.text = "\(temp)"
         }
-        if (flag == 3){
+        if flag == 3 {
             temp = temp * Double(zxc.text!)!
-            zxc.text = "\(temp)"
         }
-        if (flag == 4){
+        if flag == 4 {
             temp = temp / Double(zxc.text!)!
-            zxc.text = "\(temp)"
         }
+        flag = 0
+        zxc.text = "\(temp)"
     }
     @IBAction func ac(_ sender: Any) {
         zxc.text = ""
+        temp = 0
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
+
+
 
