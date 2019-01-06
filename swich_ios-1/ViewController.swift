eb335decb5d55395419a92c8ed8a3df15141ef9b
Zhng990317 <zbq990317@yeah.net>
2019-01-06 11:06:33 +0800

    1.6.1.2

--- swich_ios-1/ViewController.swift
+++ swich_ios-1/ViewController.swift
@@ -11,86 +11,86 @@ import UIKit
 class ViewController:UIViewController {
     
     
-    @IBOutlet weak var zxc: UITextField!
+    @IBOutlet weak var show: UITextField!
     var temp : Double = 0.0
     var flag = 0
     
     @IBAction func dot(_ sender: Any) {
-        zxc.text = zxc.text! + "."
+        show.text = show.text! + "."
     }
     @IBAction func b0 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "0"
+        show.text = show.text! + "0"
         
     }
     @IBAction func b1 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "1"
+        show.text = show.text! + "1"
         
     }
     @IBAction func b2 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "2"
+        show.text = show.text! + "2"
     }
     @IBAction func b3 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "3"
+        show.text = show.text! + "3"
         
     }
     @IBAction func b4 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "4"
+        show.text = show.text! + "4"
         
     }
     @IBAction func b5 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "5"
+        show.text = show.text! + "5"
         
     }
     @IBAction func b6 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "6"
+        show.text = show.text! + "6"
         
     }
     @IBAction func b7 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "7"
+        show.text = show.text! + "7"
         
     }
     @IBAction func b8 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "8"
+        show.text = show.text! + "8"
         
     }
     @IBAction func b9 (_ sender: Any) {
         
-        zxc.text = zxc.text! + "9"
+        show.text = show.text! + "9"
         
     }
     
     @IBAction func add(_ sender: Any) {
         
         if flag == 1 {
-            temp = temp + Double(zxc.text!)!
-            zxc.text = ""
+            temp = temp + Double(show.text!)!
+            show.text = ""
         }
         else {
-            temp = Double(zxc.text!)!
-            zxc.text = "\(temp)"
-            zxc.text = ""
+            temp = Double(show.text!)!
+            show.text = "\(temp)"
+            show.text = ""
         }
         flag = 1
     }
     @IBAction func pre(_ sender: Any) {
         
         if flag == 2 {
-            temp = temp - Double(zxc.text!)!
-            zxc.text = "\(temp)"
-            zxc.text = ""
+            temp = temp - Double(show.text!)!
+            show.text = ""
         }
         else {
-            temp = Double(zxc.text!)!
-            zxc.text = ""
+            temp = Double(show.text!)!
+            show.text = "\(temp)"
+            show.text = ""
         }
         flag = 2
         
@@ -98,13 +98,13 @@ class ViewController:UIViewController {
     @IBAction func mvl(_ sender: Any) {
         
         if flag == 3 {
-            temp = temp * Double(zxc.text!)!
-            zxc.text = "\(temp)"
-            zxc.text = ""
+            temp = temp * Double(show.text!)!
+            show.text = ""
         }
         else {
-            temp = Double(zxc.text!)!
-            zxc.text = ""
+            temp = Double(show.text!)!
+            show.text = "\(temp)"
+            show.text = ""
         }
         flag = 3
         
@@ -112,45 +112,45 @@ class ViewController:UIViewController {
     @IBAction func div(_ sender: Any) {
         
         if flag == 4 {
-            temp = temp / Double(zxc.text!)!
-            zxc.text = "\(temp)"
-            zxc.text = ""
+            temp = temp / Double(show.text!)!
+            show.text = ""
         }
         else {
-            temp = Double(zxc.text!)!
-            zxc.text = ""
+            temp = Double(show.text!)!
+            show.text = "\(temp)"
+            show.text = ""
         }
         flag = 4
         
     }
     
     @IBAction func precent(_ sender: Any) {
-        temp = Double(zxc.text!)! / 100
-        zxc.text = "\(temp)"
+        temp = Double(show.text!)! / 100
+        show.text = "\(temp)"
     }
     @IBAction func plus_minus(_ sender: Any) {
-        temp = Double(zxc.text!)! * (-1)
-        zxc.text = "\(temp)"
+        temp = Double(show.text!)! * (-1)
+        show.text = "\(temp)"
     }
     
     @IBAction func result (_ sender: Any) {
         if flag == 1 {
-            temp = temp + Double(zxc.text!)!
+            temp = temp + Double(show.text!)!
         }
         if flag == 2 {
-            temp = temp - Double(zxc.text!)!
+            temp = temp - Double(show.text!)!
         }
         if flag == 3 {
-            temp = temp * Double(zxc.text!)!
+            temp = temp * Double(show.text!)!
         }
         if flag == 4 {
-            temp = temp / Double(zxc.text!)!
+            temp = temp / Double(show.text!)!
         }
         flag = 0
-        zxc.text = "\(temp)"
+        show.text = "\(temp)"
     }
     @IBAction func ac(_ sender: Any) {
-        zxc.text = ""
+        show.text = ""
         temp = 0
     }
     override func viewDidLoad() {
