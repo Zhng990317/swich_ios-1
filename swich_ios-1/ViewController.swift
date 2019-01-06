df953d6c6f607c9ea3232425828789569a38daaf
Zhng990317 <zbq990317@yeah.net>
2019-01-06 10:38:09 +0800

    1.6.1

--- swich_ios-1/ViewController.swift
+++ swich_ios-1/ViewController.swift
@@ -16,146 +16,76 @@ class ViewController:UIViewController {
     var flag = 0
     
     @IBAction func dot(_ sender: Any) {
-            zxc.text = zxc.text! + "."
+        zxc.text = zxc.text! + "."
     }
     @IBAction func b0 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "0"
         
     }
     @IBAction func b1 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "1"
         
     }
     @IBAction func b2 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "2"
     }
     @IBAction func b3 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "3"
         
     }
     @IBAction func b4 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "4"
         
     }
     @IBAction func b5 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "5"
         
     }
     @IBAction func b6 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "6"
         
     }
     @IBAction func b7 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "7"
         
     }
     @IBAction func b8 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "8"
         
     }
     @IBAction func b9 (_ sender: Any) {
-        if zxc.text == "0"
-        {
-            zxc.text = ""
-        }
+        
         zxc.text = zxc.text! + "9"
         
     }
     
     @IBAction func add(_ sender: Any) {
         
-        if zxc.text != "" {
-            if flag == 2 {
-                temp = temp - Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 3 {
-                temp = temp * Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-             }
-            if flag == 4 {
-                temp = temp / Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-             }
-        }
-        
         if flag == 1 {
-            if zxc.text == "" {
-                zxc.text = "0"
-            }
             temp = temp + Double(zxc.text!)!
             zxc.text = ""
         }
         if flag == 0 {
             temp = Double(zxc.text!)!
+            zxc.text = "\(temp)"
             zxc.text = ""
         }
         flag = 1
     }
     @IBAction func pre(_ sender: Any) {
-       
-        if zxc.text != "" {
-            if flag == 1 {
-                temp = temp + Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 3 {
-                temp = temp * Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 4 {
-                temp = temp / Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-        }
         
         if flag == 2 {
-            if zxc.text == "" {
-                zxc.text = "0"
-            }
             temp = temp - Double(zxc.text!)!
+            zxc.text = "\(temp)"
             zxc.text = ""
         }
         if flag == 0 {
@@ -167,29 +97,9 @@ class ViewController:UIViewController {
     }
     @IBAction func mvl(_ sender: Any) {
         
-        if zxc.text != ""{
-            if flag == 1 {
-                temp = temp + Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 2 {
-                temp = temp - Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 4 {
-                temp = temp / Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-        }
-        
         if flag == 3 {
-            if zxc.text == "" {
-                zxc.text = "3"
-            }
             temp = temp * Double(zxc.text!)!
+            zxc.text = "\(temp)"
             zxc.text = ""
         }
         if flag == 0 {
@@ -200,30 +110,10 @@ class ViewController:UIViewController {
         
     }
     @IBAction func div(_ sender: Any) {
-       
-        if zxc.text != "" {
-            if flag == 1 {
-                temp = temp + Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 2 {
-                temp = temp - Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-            if flag == 3 {
-                temp = temp * Double(zxc.text!)!
-                zxc.text = "\(temp)"
-                zxc.text = ""
-            }
-        }
         
         if flag == 4 {
-            if zxc.text == "" {
-                zxc.text = "1"
-            }
             temp = temp / Double(zxc.text!)!
+            zxc.text = "\(temp)"
             zxc.text = ""
         }
         if flag == 0 {
